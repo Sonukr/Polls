68206ea71a599ff51dd6fce0a7f681032e8d8f22
Sonukr <sonuyadav9756@gmail.com>
2017-04-19 12:24:58 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :comments
+  get 'pages/info'
+
+  resources :ideas
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/ideas')
+end
