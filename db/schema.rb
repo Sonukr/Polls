33781c452c5814c77a60782121dae33be36e7e3b
Sonukr <sonuyadav9756@gmail.com>
2017-05-03 00:18:09 +0530

    login using Fb done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419163059) do
+ActiveRecord::Schema.define(version: 20170502183933) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20170419163059) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "gender"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
