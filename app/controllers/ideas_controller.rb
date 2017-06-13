bce1deceac3fd667796061da247610d974559bd4
Sonukr <sonuyadav9756@gmail.com>
2017-06-14 00:08:12 +0530

    current user added to idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,7 +4,7 @@ class IdeasController < ApplicationController
   # GET /ideas
   # GET /ideas.json
   def index
-    @ideas = Idea.all
+    @ideas = Idea.where(:user_id => current_user.id)
   end
 
   # GET /ideas/1
@@ -27,6 +27,7 @@ class IdeasController < ApplicationController
   # POST /ideas.json
   def create
     @idea = Idea.new(idea_params)
+    @idea.user_id = current_user.id
 
     respond_to do |format|
       if @idea.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502183933) do
+ActiveRecord::Schema.define(version: 20170613180920) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170502183933) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|
